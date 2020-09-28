44d3be5effd4212b665d27f259758bcc660d4997
yangrenjun <54614002@qq.com>
2020-09-28 17:27:54 +0800

    add:添加文件夹

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import './plugins/iview.js'
+import './plugins/iview.ts'
 
 Vue.config.productionTip = false
 
